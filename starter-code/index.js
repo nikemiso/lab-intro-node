2d777b1641381a767b1824cb1172d42ad5072f4d
Nike Michel-Soth <nikemichelsoth@gmail.com>
2018-12-06 21:39:51 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  add(item) {
+   this.length++;
+   this.items.push(item);
+   this.items.sort((a,b) => a-b)
+
+  }
+  get(pos) {
+    return this.items[pos-1];
+
+  }
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else { return this.items[this.length-1]}
+    
+  }
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else { return this.items[0]}
+
+  }
+
+  average() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {return this.sum()/this.length} 
+    
+  }
+
+  sum() {
+    return this.items.reduce(function(acc, curr) {
+    return acc + curr;},0)
 };
+}
 
 module.exports = SortedList;
+
